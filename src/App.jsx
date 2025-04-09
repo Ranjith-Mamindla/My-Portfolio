b751311ee2da53adacf86ee9635111dee83abeac
Ranjith-Mamindla <ranjithmamindla02@gmail.com>
2025-04-09 12:34:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
